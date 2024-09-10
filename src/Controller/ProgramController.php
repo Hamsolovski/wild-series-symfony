34f19661ab3d61c0f52d5792e3d976dd65a49439
Antoine DELALANDE <antoine.delalande@fne.asso.fr>
2024-09-10 15:59:38 +0200

    done advanced routing quest

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -15,4 +15,13 @@ class ProgramController extends AbstractController
             'website' => 'Wild Series',
         ]);
     }
+
+    #[Route('/program/{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'program_{id}')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'website' => 'Wild Series',
+            'id' => $id,
+        ]);
+    }
 }
