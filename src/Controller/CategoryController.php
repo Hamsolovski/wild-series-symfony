c55762f1804a9e8aa057b02cae2358d9036421ad
Antoine Delalande <ant.delalande@gmail.com>
2024-09-17 16:09:25 +0200

    added program routes

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/category', name: 'category_index')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('category/index.html.twig', [
+            'website' => 'Wild Series',
+            'categories' => $categories,
+        ]);
+    }
+}
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,23 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProgramRepository;
 
 class ProgramController extends AbstractController
 {
     #[Route('/program', name: 'program_index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
+
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
+            'programs' => $programs,
         ]);
     }
 
     #[Route('/program/{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'program_{id}')]
-    public function show(int $id): Response
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
+        $program = $programRepository->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id ' . $id . ' found in program\'s table'
+            );
+        }        
+    
         return $this->render('program/show.html.twig', [
             'website' => 'Wild Series',
-            'id' => $id,
+            'program' => $program,
         ]);
     }
 }
